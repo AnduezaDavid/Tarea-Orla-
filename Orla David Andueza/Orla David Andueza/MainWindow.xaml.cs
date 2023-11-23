cc9cf7a32e17b833eb0a0a0a891710a99e45d71b
AnduezaDavid <149382544+AnduezaDavid@users.noreply.github.com>
2023-11-23 20:56:31 +0100

    Orla Definitiva

---
+++ Orla David Andueza/Orla David Andueza/MainWindow.xaml.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Orla_David_Andueza
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MouseEnter41(object sender, MouseEventArgs e)
+        {
+            LabelPuesto41.Text = "Aitor Cuadrado Aramendía";
+        }
+        private void MouseLeave41(object sender, MouseEventArgs e)
+        {
+            LabelPuesto41.Text = "";
+        }
+        private void MouseClick41(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto41.Text = "acuadraara1@educacion.navarra.es";
+        }
+        private void MouseDoubleClick41(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Aitor", "Cuadrado Aramendía", "acuadraara1@educacion.navarra.es", "Imagenes/Aitor.png");
+            win2.Show();
+        }
+
+        private void MouseEnter42(object sender, MouseEventArgs e)
+        {
+            LabelPuesto42.Text = "Joseba Trigo Sierra";
+        }
+        private void MouseLeave42(object sender, MouseEventArgs e)
+        {
+            LabelPuesto42.Text = "";
+        }
+        private void MouseClick42(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto42.Text = "jtrigosier1@educacion.navarra.es";
+        }
+        private void MouseDoubleClick42(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Joseba", "Trigo Sierra", "jtrigosier1@educacion.navarra.es", "Imagenes/Joseba.png");
+            win2.Show();
+        }
+
+        private void MouseEnter43(object sender, MouseEventArgs e)
+        {
+            LabelPuesto43.Text = "Cristina López Lusarreta";
+        }
+        private void MouseLeave43(object sender, MouseEventArgs e)
+        {
+            LabelPuesto43.Text = "";
+        }
+        private void MouseClick43(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto43.Text = "clopezlusa1@educacion.navarra.es";
+        }
+        private void MouseDoubleClick43(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Cristina", "López Lusarreta", "clopezlusa1@educacion.navarra.es", "Imagenes/Cristina.png");
+            win2.Show();
+        }
+
+        private void MouseEnter44(object sender, MouseEventArgs e)
+        {
+            LabelPuesto44.Text = "Rubén Gómez García";
+        }
+        private void MouseLeave44(object sender, MouseEventArgs e)
+        {
+            LabelPuesto44.Text = "";
+        }
+        private void MouseClick44(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto44.Text = "rgomezgarc@educacion.navarra.es";
+        }
+        private void MouseDoubleClick44(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Rubén", "Gómez García", "rgomezgarc@educacion.navarra.es", "Imagenes/Ruben.png");
+            win2.Show();
+        }
+
+        private void MouseEnter31(object sender, MouseEventArgs e)
+        {
+            LabelPuesto31.Text = "Mario Puente Berasategui";
+        }
+        private void MouseLeave31(object sender, MouseEventArgs e)
+        {
+            LabelPuesto31.Text = "";
+        }
+        private void MouseClick31(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto31.Text = "mpuenteber11@educacion.navarra.es";
+        }
+        private void MouseDoubleClick31(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Mario", "Puente Berasategui", "mpuenteber11@educacion.navarra.es", "Imagenes/Mario.png");
+            win2.Show();
+        }
+
+        private void MouseEnter32(object sender, MouseEventArgs e)
+        {
+            LabelPuesto32.Text = "Jagoba Inda Arizaleta";
+        }
+        private void MouseLeave32(object sender, MouseEventArgs e)
+        {
+            LabelPuesto32.Text = "";
+        }
+        private void MouseClick32(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto32.Text = "jindaariza1@educacion.navarra.es";
+        }
+        private void MouseDoubleClick32(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Jagoba", "Inda Arizaleta", "jindaariza1@educacion.navarra.es", "Imagenes/Jagoba.png");
+            win2.Show();
+        }
+
+        private void MouseEnter33(object sender, MouseEventArgs e)
+        {
+            LabelPuesto33.Text = "Endika Eguino Garbayo";
+        }
+        private void MouseLeave33(object sender, MouseEventArgs e)
+        {
+            LabelPuesto33.Text = "";
+        }
+        private void MouseClick33(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto33.Text = "eeguinogar@educacion.navarra.es";
+        }
+        private void MouseDoubleClick33(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Endika", "Eguino Garbayo", "eeguinogar@educacion.navarra.es", "Imagenes/Endika.png");
+            win2.Show();
+        }
+
+        private void MouseEnter34(object sender, MouseEventArgs e)
+        {
+            LabelPuesto34.Text = "Santiago Lerga Cambra";
+        }
+        private void MouseLeave34(object sender, MouseEventArgs e)
+        {
+            LabelPuesto34.Text = "";
+        }
+        private void MouseClick34(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto34.Text = "slergacamb@educacion.navarra.es";
+        }
+        private void MouseDoubleClick34(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Santiago", "Lerga Cambra", "slergacamb@educacion.navarra.es", "Imagenes/Santiago.png");
+            win2.Show();
+        }
+
+        private void MouseEnter21(object sender, MouseEventArgs e)
+        {
+            LabelPuesto21.Text = "Julen Baztarrica Puyal";
+        }
+        private void MouseLeave21(object sender, MouseEventArgs e)
+        {
+            LabelPuesto21.Text = "";
+        }
+        private void MouseClick21(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto21.Text = "jbaztarpuy@educacion.navarra.es";
+        }
+        private void MouseDoubleClick21(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Julen", "Baztarrica Puyal", "jbaztarpuy@educacion.navarra.es", "Imagenes/Julen.png");
+            win2.Show();
+        }
+
+        private void MouseEnter22(object sender, MouseEventArgs e)
+        {
+            LabelPuesto22.Text = "David Andueza Ferro";
+        }
+        private void MouseLeave22(object sender, MouseEventArgs e)
+        {
+            LabelPuesto22.Text = "";
+        }
+        private void MouseClick22(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto22.Text = "danduezfer@educacion.navarra.es";
+        }
+        private void MouseDoubleClick22(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("David", "Andueza Ferro", "danduezfer@educacion.navarra.es", "Imagenes/David.png");
+            win2.Show();
+        }
+
+        private void MouseEnter23(object sender, MouseEventArgs e)
+        {
+            LabelPuesto23.Text = "Almudena Iparraguirre Castillo";
+        }
+        private void MouseLeave23(object sender, MouseEventArgs e)
+        {
+            LabelPuesto23.Text = "";
+        }
+        private void MouseClick23(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto23.Text = "aiparracas1@educacion.navarra.es";
+        }
+        private void MouseDoubleClick23(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Almudena", "Iparraguirre Castillo", "aiparracas1@educacion.navarra.es", "Imagenes/Almudena.png");
+            win2.Show();
+        }
+
+        private void MouseEnter24(object sender, MouseEventArgs e)
+        {
+            LabelPuesto24.Text = "Aitor Zubillaga Soria";
+        }
+        private void MouseLeave24(object sender, MouseEventArgs e)
+        {
+            LabelPuesto24.Text = "";
+        }
+        private void MouseClick24(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto24.Text = "azubillsor@educacion.navarra.es";
+        }
+        private void MouseDoubleClick24(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Aitor", "Zubillaga Soria", "azubillsor@educacion.navarra.es", "Imagenes/AitorZ.png");
+            win2.Show();
+        }
+
+        private void MouseEnter11(object sender, MouseEventArgs e)
+        {
+            LabelPuesto11.Text = "Rubén López-Davalillo Ledesma";
+        }
+        private void MouseLeave11(object sender, MouseEventArgs e)
+        {
+            LabelPuesto11.Text = "";
+        }
+        private void MouseClick11(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto11.Text = "rlopezdled@educacion.navarra.es";
+        }
+        private void MouseDoubleClick11(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Rubén", "López-Davalillo Ledesma", "rlopezdled@educacion.navarra.es", "Imagenes/RubenL.png");
+            win2.Show();
+        }
+
+        private void MouseEnter12(object sender, MouseEventArgs e)
+        {
+            LabelPuesto12.Text = "Beñat Martínez Moreno";
+        }
+        private void MouseLeave12(object sender, MouseEventArgs e)
+        {
+            LabelPuesto12.Text = "";
+        }
+        private void MouseClick12(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto12.Text = "bmartinmor3@educacion.navarra.es";
+        }
+        private void MouseDoubleClick12(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Beñat", "Martínez Moreno", "bmartinmor3@educacion.navarra.es", "Imagenes/Beñat.png");
+            win2.Show();
+        }
+
+        private void MouseEnter13(object sender, MouseEventArgs e)
+        {
+            LabelPuesto13.Text = "Aimar Urquizu Diego";
+        }
+        private void MouseLeave13(object sender, MouseEventArgs e)
+        {
+            LabelPuesto13.Text = "";
+        }
+        private void MouseClick13(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto13.Text = "aurquizdie1@educacion.navarra.es";
+        }
+        private void MouseDoubleClick13(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Aimar", "Urquizu Diego", "aurquizdie1@educacion.navarra.es", "Imagenes/Aimar.png");
+            win2.Show();
+        }
+
+        private void MouseEnter14(object sender, MouseEventArgs e)
+        {
+            LabelPuesto14.Text = "Midgard Gastón Almeida Minda";
+        }
+        private void MouseLeave14(object sender, MouseEventArgs e)
+        {
+            LabelPuesto14.Text = "";
+        }
+        private void MouseClick14(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto14.Text = "malmeidmin@educacion.navarra.es";
+        }
+        private void MouseDoubleClick14(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("Midgard Gastón", "Almeida Minda", "malmeidmin@educacion.navarra.es", "Imagenes/Midgard.png");
+            win2.Show();
+        }
+
+        private void MouseEnter01(object sender, MouseEventArgs e)
+        {
+            LabelPuesto01.Text = "David Gurucharri";
+        }
+        private void MouseLeave01(object sender, MouseEventArgs e)
+        {
+            LabelPuesto01.Text = "";
+        }
+        private void MouseClick01(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto01.Text = "dguruchazc@educacion.navarra.es";
+        }
+        private void MouseDoubleClick01(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1("David", "Gurucharri", "dguruchazc@educacion.navarra.es", "Imagenes/DavidG.jpeg");
+            win2.Show();
+        }
+
+    }
+}
